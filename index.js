13cac7bab22153a10dc2ea4de87d5f1befc59088
José Antonio Vásquez <jvasquez978@gmail.com>
2025-03-28 20:54:01 -0400

    Agrgegando docker compose

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 import express from "express";
 import axios from "axios";
 import responseTime from "response-time";
+import {createClient} from "redis";
 
 
 const app = express();
+
+const client = createClient({
+    host: 'localhost',
+    port: 6379
+})
 
 app.use(responseTime());
 
 app.get("/characters", async (req, res) => {
 
+
+   const replay = await client.get('characters') 
+
+   if(replay) return res.json(JSON.parse(replay))
+    
+
    const {data} = await axios.get('https://rickandmortyapi.com/api/character')
+
+   const saveResult = await client.set('characters', JSON.stringify(data))
+   console.log(saveResult)
 
    return res.json(data)
     
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+
+app.get("/characters/:id", async (req, res) => {
+      const {id} = req.params;
+      const replay = await client.get(`character:${id}`)
+      if(replay) return res.json(JSON.parse(replay))
+      const {data} = await axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+      const saveResult = await client.set(`character:${id}`, JSON.stringify(data))
+      console.log(saveResult)
+      return res.json(data)
 })
+
+
+
+const main = async () => {
+    await client.connect();
+    app.listen(3000);
+    console.log('Server is running on port 3000')
+}
+main();
